46a017ec61eb1bece7ebf3845240653a7f241558
Alissonaguiarpires <alissonpiresaguiar@gmail.com>
2026-06-12 14:24:58 -0300

    Novas tarefas

--- ex007/ex002.js
+++ ex007/ex002.js
@@ -1,12 +1,16 @@
 var agora = new Date()
 var hora = agora.getHours()
 console.log (`Agora são exatamente ${hora} horas.`)
-if (hora < 12){
+if (hora < 12  && hora > 7){
     console.log('Bom dia')
 }else{
-    if (hora <= 18){
+    if (hora <= 18  || hora >23){
         console.log('Boa tarde')
+    }else{
+        if (hora <= 5 && hora >= 1) {
+        console.log('Boa madrugada')
     }else{
         console.log('Boa noite')
     }
+    } 
 }
