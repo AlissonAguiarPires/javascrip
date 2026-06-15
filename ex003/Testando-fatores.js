64d346365b2909c84c66678ea1f4869122b4c17e
Alissonaguiarpires <alissonpiresaguiar@gmail.com>
2026-06-15 14:05:48 -0300

    ex

---
+++ ex003/Testando-fatores.js
@@ -0,0 +1,12 @@
+
+var a = ('Bom dia')
+
+if (a == 'Bom dia'){
+    console.log('Bom dia')
+}else if (a == 'Boa tarde'){
+    console.log('Boa Tarde')
+}else if (a =='Boa Noite'){
+    console.log('Boa noite')
+}else {
+    console.log('Hora invalida')
+}
---
+++ ex003/operadore.js
@@ -0,0 +1,8 @@
+ /*var a =true && false
+ console.log(`${a}`)*/
+ /*var a = false || true
+ console.log(`${a}`)*/
+ bom = false
+ ruim = false
+ var a = bom || ruim
+ console.log (`${a}`)
