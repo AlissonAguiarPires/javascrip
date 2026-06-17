a74c80c97bff982b876dc14343deb07147e2a972
Alissonaguiarpires <alissonpiresaguiar@gmail.com>
2026-06-17 20:59:59 -0300

    ex009 e ex010

--- ex009/script.js
+++ ex009/script.js
@@ -3,7 +3,6 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
-
     if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
@@ -11,12 +10,9 @@ function verificar() {
         var idade = ano - Number(fano.value)
         var gênero = ''
         var img = document.createElement('img')
-
         img.setAttribute('id', 'foto')
-
         if (fsex[0].checked) {
             gênero = 'Homem'
-
             if (idade >= 0 && idade < 10) {
                 // Criança
                 img.setAttribute('src', 'bh.jpg')
@@ -30,25 +26,18 @@ function verificar() {
                 // Idoso
                 img.setAttribute('src', 'ih.jpg')
             }
-
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
-
             if (idade >= 0 && idade < 10) {
-                // Criança
                 img.setAttribute('src', 'bm.jpg')
             } else if (idade < 21) {
-                // Jovem
                 img.setAttribute('src', 'jm.jpg')
             } else if (idade < 50) {
-                // Adulto
                 img.setAttribute('src', 'jm.jpg')
             } else {
-                // Idoso
                 img.setAttribute('src', 'im.jpg')
             }
         }
-
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
         res.appendChild(img)
---
+++ ex010/repetção.js
@@ -0,0 +1,10 @@
+/*var c =1
+while (c <= 100){
+    console.log('Tudo bem')
+    c++ // c = c + 1
+}*/
+var c =1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}while( c <= 600)
